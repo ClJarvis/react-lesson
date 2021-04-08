db61c7db8040bf142c40c68cb13ee76480ba679c
Chris Jarvis <clj@ollure.com>
2021-04-08 15:14:05 -0500

    Update button.jsx

--- button.jsx
+++ button.jsx
@@ -10,3 +10,35 @@ ReactDOM.render(
   <Button />, 
   document.getElementById('mountNode'),
 );
+
+/////
+function Button(props) {
+//	const [counter, setCounter] = useState(0);
+	return (
+    <button onClick={props.onClickFunction}>
+      +1
+    </button>
+    );
+}
+
+function Display(props) {
+  return (
+    <div>{props.message}</div>
+  );
+}
+
+function App() {
+  const [counter, setCounter] = useState(42);
+  const incrementCounter = () => setCounter(counter+1);
+  return (
+  <div>
+      <Button onClickFunction={incrementCounter}/>
+      <Display message={counter}/>
+  </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('mountNode'),
+);
