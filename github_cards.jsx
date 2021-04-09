18480a660bc147e3b21c4a4c268f0cc88c438f10
Chris Jarvis <clj@ollure.com>
2021-04-09 15:55:23 -0500

    Update github_cards.jsx

--- github_cards.jsx
+++ github_cards.jsx
@@ -2,7 +2,7 @@ class Card extends React.Component {
   render() {
     return (
     <div className="gitHub-Profile">
-        <img src="https://placehold.it/75" />
+               <img src="https://via.placeholder.com/75" />
         <div className="info">
           <div className="name">Name here ..</div>
           <div className="company">Company here...</div>
