2c1bd365672dd7f80094e42956c910a0fae0cd14
Chris Jarvis <clj@ollure.com>
2021-04-09 15:11:42 -0500

    Create github_cards.jsx

---
+++ github_cards.jsx
@@ -0,0 +1,34 @@
+class Card extends React.Component {
+  render() {
+    return (
+    <div className="gitHub-Profile">
+        <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">Name here ..</div>
+          <div className="company">Company here...</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+   return (
+     <div>
+     <div className='header'>{this.props.title}</div>
+     <Card />
+     </div>
+    );
+  }
+}
+
+
+//const App = ({title}) => (
+//  <div className="header">{title}</div>
+// );
+
+ReactDOM.render(
+	<App title="The GitHub Cards App" />,
+  mountNode,
+);
