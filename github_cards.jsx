be6480ffe5babadcd90cb77ccd01779101a7c88f
Chris Jarvis <clj@ollure.com>
2021-04-11 14:19:00 -0500

    Update github_cards.jsx

--- github_cards.jsx
+++ github_cards.jsx
@@ -1,32 +1,58 @@
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+const CardList = (props) => (
+	<div>
+  	{props.profiles.map(profile => <Card {...profile}/>)}
+	</div>
+);
+
 class Card extends React.Component {
+	render() {
+  	const profile = this.props;
+  	return (
+    	<div className="github-profile">
+    	  <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+    	</div>
+    );
+  }
+}
+
+class Form extends React.Component {
   render() {
     return (
-    <div className="gitHub-Profile">
-               <img src="https://via.placeholder.com/75" />
-        <div className="info">
-          <div className="name">Name here ..</div>
-          <div className="company">Company here...</div>
-        </div>
-      </div>
-    );
+      <form action="">
+        <input type="text" placeholder="GitHub username" />
+        <button>Add card</button>
+      </form>
+    )
   }
 }
 
 class App extends React.Component {
-  render() {
-   return (
-     <div>
-     <div className='header'>{this.props.title}</div>
-     <Card />
-     </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      profiles: testData,
+    };
   }
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form />
+        <CardList profiles={this.state.profiles} />
+    	</div>
+    );
+  }	
 }
-
-
-//const App = ({title}) => (
-//  <div className="header">{title}</div>
-// );
 
 ReactDOM.render(
 	<App title="The GitHub Cards App" />,
